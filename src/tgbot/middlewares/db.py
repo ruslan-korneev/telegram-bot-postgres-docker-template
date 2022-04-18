a117d8ca7b67195bd8541cd4a34380941c93150c
Ruslan <kornerus@fjfi.cvut.cz>
2022-04-18 19:32:21 +0300

    refactoring

--- src/tgbot/middlewares/db.py
+++ src/tgbot/middlewares/db.py
@@ -5,6 +5,7 @@ class DbMiddleware(LifetimeControllerMiddleware):
     skip_patterns = ["error", "update"]
 
     async def pre_process(self, obj, data, *args):
-        db_session = obj.bot.get('db')
+        pass
+        # db_session = obj.bot.get('db')
         # Get data from table to handler
         # data['some_model'] = await Model.get()
